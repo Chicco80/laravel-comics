a44167c274a7cdc4a8673d9d04a1fc15f610ff3e
Chicco80 <chicco80lisa@gmail.com>
2022-06-10 15:49:28 +0200

    inizio

---
+++ resources/views/home.blade.php
@@ -0,0 +1,92 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+    <title>dc-comics</title>
+</head>
+<body>
+    <header>
+        <div class="container-fluid">
+            <nav class="pt-3">
+                <div class="container d-flex justify-content-between">
+                    <div class="logo">
+                        <img src="images/dc-logo.png" alt="">
+                    </div>
+                    <div class="links">
+                        <ul class="d-flex py-3 ">
+                            <li><a href="">CHARACTERS</a></li>
+                            <li><a href="">COMICS</a></li>
+                            <li><a href="">MOVIES</a></li>
+                            <li><a href="">TV</a></li>
+                            <li><a href="">GAMES</a></li>
+                            <li><a href="">COLLECTIBLES</a></li>
+                            <li><a href="">VIDEOS</a></li>
+                            <li><a href="">FANS</a></li>
+                            <li><a href="">NEWS</a></li>
+                            <li><a href="">SHOP</a></li>
+                        </ul>
+                    </div>
+                    <div class="search">
+                        <input class="my-4" type="search" name="search" id="">
+                        <input class="my-4" type="button" value="search" id="search">
+                    </div>
+                </div>
+            </nav>
+        </div>
+        
+    </header>
+    <div class="jumbo">
+        <img src="images/jumbotron.jpg" alt="">
+    </div>
+
+    <main>
+
+    </main>
+
+    <footer>
+
+    </footer>
+    
+</body>
+</html>
+
+<style>
+    nav{
+        width: 100%;
+        height: 100px;
+        position: fixed;
+
+        
+    }
+    .container-fluid{
+        background-color: white;
+        padding-bottom: 
+        
+    }
+    .logo img{
+        width:80px;
+        height: 80px;
+    }
+    ul{
+        list-style-type: none;
+        align-items: center;
+       
+    }
+    li{
+        padding:10px
+    }
+    a{
+        text-decoration: none;
+        color: black
+    }
+    .jumbo img{
+        padding-top: 100px;
+        width: 100%;
+        height:500px;
+        
+    }
+
+</style>
