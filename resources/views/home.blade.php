c6a50f4b2b3c93f975146f475e4926be940ad606
Chicco80 <chicco80lisa@gmail.com>
2022-06-10 20:25:03 +0200

    ok main

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,6 +8,9 @@
 @section('mainContent')
 <div class="container-fluid bg-dark">
     <div class="container py-5">
+        <div class="button-up ">
+            <input class="  bg-primary p-2 text-white" type="button" value="CURRENT SERIES">
+        </div>
         <div class="row">
             @foreach ($comic as $item)
             <div class="col-2 my-3">
@@ -20,9 +23,38 @@
             </div>
             @endforeach
         </div>
+        <div class="button py-3 d-flex justify-content-center">
+            <input class="  bg-primary p-2 text-white" type="button" value="LOAD MORE">
+        </div>
     </div>
 </div>
+<div class="container-fluid bg-primary">
+    <div class="container">
+        <div class="row">
+            @foreach($icon as $i)
+            <div class="col py-5">
+                <img  class="icon" src="{{$i['src']}}" alt="">
+                <span class="text-white">{{$i['title']}}</span>
+            </div>
+            @endforeach
+        </div>
+    </div>
+</div>
+
 <style>
+
+    .container-fluid{
+        position: relative;
+    }
+    .button-up{
+        position: absolute;
+        top:-20px;
+        left: 300px;
+    }
+    .icon{
+        width: 50px;
+        height: 50px;
+    }
 
 </style>
     
--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,8 @@ use Illuminate\Support\Facades\Route;
 */
 Route::get('/', function () {
     $comic = config('comics');
-    return view('home',['comic'=>$comic]);
+    $icons = config('icons');
+    return view('home',['comic'=>$comic],  ['icon'=> $icons]);
     
 });
 
