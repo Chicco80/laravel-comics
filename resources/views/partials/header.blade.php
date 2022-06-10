607be9e3302d98dfbbe7b322ee9e0e87f3e1a319
Chicco80 <chicco80lisa@gmail.com>
2022-06-10 16:26:49 +0200

    include

---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,33 @@
+<header>
+    <div class="container-fluid">
+        <nav class="pt-3">
+            <div class="container d-flex justify-content-between">
+                <div class="logo">
+                    <a href="/"><img src="images/dc-logo.png" alt=""></a>
+                </div>
+                <div class="links">
+                    <ul class="d-flex py-3 ">
+                        <li><a href="#">CHARACTERS</a></li>
+                        <li><a href="#">COMICS</a></li>
+                        <li><a href="#">MOVIES</a></li>
+                        <li><a href="#">TV</a></li>
+                        <li><a href="#">GAMES</a></li>
+                        <li><a href="#">COLLECTIBLES</a></li>
+                        <li><a href="#">VIDEOS</a></li>
+                        <li><a href="#">FANS</a></li>
+                        <li><a href="#">NEWS</a></li>
+                        <li><a href="/shop">SHOP</a></li>
+                    </ul>
+                </div>
+                <div class="search">
+                    <input class="my-4" type="search" name="search" id="">
+                    <input class="my-4" type="button" value="search" id="search">
+                </div>
+            </div>
+        </nav>
+    </div>
+    
+</header>
+<div class="jumbo">
+    <img src="images/jumbotron.jpg" alt="">
+</div>
